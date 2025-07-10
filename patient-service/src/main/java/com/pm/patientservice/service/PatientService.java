81b65c3eab22062a9f7ba97c86033c7b1e18c9fd
asif-0007 <mohdasif4949@gmail.com>
2025-07-10 15:26:48 +0530

    added producer code for kafka

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -18,11 +19,13 @@ import java.util.UUID;
 public class PatientService {
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
     public PatientService(PatientRepository patientRepository
-    , BillingServiceGrpcClient billingServiceGrpcClient) {
+    , BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatients (){
@@ -36,10 +39,14 @@ public class PatientService {
             throw new EmailAlreadyExistsException("A patient with this email "
              + "already exists " + patientRequestDTO.getEmail());
         }
+
         Patient newPatient = patientRepository.save(
                 PatientMapper.toPatientEntityModel(patientRequestDTO));
+
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
                 newPatient.getName(),newPatient.getEmail());
+
+        kafkaProducer.sendEvent(newPatient);
         return PatientMapper.toPatientResponseDTO(newPatient);
     }
     public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
